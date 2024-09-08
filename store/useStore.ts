0eee0bad7ac859bd5ee0184591228316c51b0a75
rahul parida <rparida1052@gmail.com>
2024-09-08 20:56:15 +0530

    Persist onboarding state in async storage

--- store/useStore.ts
+++ store/useStore.ts
@@ -1,19 +1,26 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { create } from "zustand";
+import { persist,createJSONStorage } from "zustand/middleware";
 
 type UserStore = {
     hasOnboarded:boolean;
     toggleOnboarding:() => void;
 }
 
-export const useUserStore = create<UserStore>((set)=>({
-    hasOnboarded:false,
-    toggleOnboarding:()=>{
-        set((state)=>{
-            console.log("state value is ",state)
-            return {
-                ...state,
-                hasOnboarded:!state.hasOnboarded
-            }
-        })
-    }
-}))
+export const useUserStore = create(
+  persist<UserStore>((set) => ({
+    hasOnboarded: false,
+    toggleOnboarding: () => {
+      set((state) => {
+        console.log("state value is ", state);
+        return {
+          ...state,
+          hasOnboarded: !state.hasOnboarded,
+        };
+      });
+    },
+  }),{
+    name:"user-storage",
+    storage:createJSONStorage(()=>AsyncStorage)
+  })
+);
