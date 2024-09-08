87e6f6c7d5f44ec0892c05b168cd54a943bf42a6
rahul parida <rparida1052@gmail.com>
2024-09-08 22:43:51 +0530

    Added custome Pressable button

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,15 @@
+import { PlantlyButton } from "@/component/plantlyButton";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const handlePress = () => {
+    console.log("Button pressed");
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <PlantlyButton title="Plant" onPress={handlePress}/>
       <StatusBar style="auto" />
     </View>
   );
--- theme.ts
+++ theme.ts
@@ -1,3 +1,4 @@
 export const theme = {
   colorGreen: "#29b365",
+  colorLeafyGreen: "#206a42",
 };
